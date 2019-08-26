4fa4b1eeea0f579be000641c40b6d9ce14d84f53
CooperNiu <niuyanno.1@outlook.com>
2019-08-26 21:17:19 +0800

    重构，通过了测试，大功告成

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -15,6 +15,61 @@ public class Main {
 
     private static final int RUN_TIMES = 6;
     
+    public static String[] isValid(String input) {
+    	String[] arr = input.split(" ");
+    	if (arr == null) {
+    		System.out.println("Wrong Input，Input again\n");
+			return null;
+		}
+        if (input.length() != 7) {
+			System.out.println("Wrong Input，Input again\n");
+			return null;
+		}
+        
+        Set<Integer> set = new HashSet<Integer>();
+        
+        for (String a : arr) {
+			set.add(Integer.parseInt(a));
+		}
+        if (set.size() != 4) {
+        	System.out.println("Wrong Input，Input again\n");
+        	return null;
+		}
+		return arr;
+	}
+    
+    public static int countAFunc(String[] arr) {
+    	int countA = 0;
+    	if (arr[0].equals("1")) {
+			countA+=1;
+		} 
+        if (arr[1].equals("2")) {
+			countA+=1;
+		} 
+        if (arr[2].equals("3")) {
+			countA+=1;
+		} 
+        if (arr[3].equals("4")) {
+			countA+=1;
+		} 
+        return countA;
+	}
+    
+    public static int countBFunc(String[] arr, int countA) {
+    	int countB = 0;
+    	for (String c : arr) {
+        	if (c.equals("1") || c.equals("2") || c.equals("3") || c.equals("4")) {
+				countB++;
+			}
+        	else {
+				continue;
+			}
+		}        
+                    
+        int B = countB-countA > 0 ? countB-countA : 0;
+        
+        return B;
+	}
 
     public static void main(String[] args) throws Exception {
     	int countA = 0;
@@ -22,53 +77,18 @@ public class Main {
         for (int i = 0; i < RUN_TIMES; i++) {
             String input = new GuessInputCommand().input();
 //            System.out.println(input.length());
-            String[] arr = input.split(" ");
-            if (input.length() != 7) {
-				System.out.println("Wrong Input，Input again\n");
-				continue;
-			}
+            String[] arr = isValid(input) ;
             
-            Set<Integer> set = new HashSet<Integer>();
-            
-            for (String a : arr) {
-				set.add(Integer.parseInt(a));
-			}
-            if (set.size() != 4) {
-            	System.out.println("Wrong Input，Input again\n");
-				continue;
-			}
            
-//            System.out.println(arr[0]);
-            if (arr[0].equals("1")) {
-				countA+=1;
-			} 
-            if (arr[1].equals("2")) {
-				countA+=1;
-			} 
-            if (arr[2].equals("3")) {
-				countA+=1;
-			} 
-            if (arr[3].equals("4")) {
-				countA+=1;
-			} 
+            countA = countAFunc(arr);
+            countB = countBFunc(arr, countA);
             
-            for (String c : arr) {
-            	if (c.equals("1") || c.equals("2") || c.equals("3") || c.equals("4")) {
-					countB++;
-				}
-            	else {
-					continue;
-				}
-			}        
-                        
-            int B = countB-countA > 0 ? countB-countA : 0;
-            System.out.println(countA+"A"+ B+"B\n");	
+            System.out.println(countA+"A"+ countB+"B\n");	
             countA = 0;
-            countB = 0;
-            B = 0;
+            countB = 0;          
         }
         
-        
+       
     }
     
     
